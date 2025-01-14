20072babdc1b2828c41f3f9c44b3d4f8be5e1ea7
Kartik Gounder <kartikgounder@gmail.com>
2025-01-14 21:37:09 +0530

    server completed

--- api/index.py
+++ api/index.py
@@ -1,1 +1,4 @@
 from app import app
+
+def handler(request):
+    return app(request)
--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, request, jsonify
+import requests
 
 # Initialize Flask app
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return jsonify({"message": "Hello, World!"}), 200
+    user_name = request.headers.get("username")
+    password = request.headers.get("password")
 
-# @app.route('/api/process', methods=['POST'])
-# def process_data():
-#     try:
-#         data = request.get_json()
-#         if not data:
-#             return jsonify({"error": "No JSON data provided"}), 400
-#         processed_data = {"received": data, "status": "Processed"}
-#         return jsonify(processed_data), 200
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
+    if user_name == "kartik" and password == "kartik":
+        return jsonify({"message": "Hello, World!"}), 200
+    else:
+        return jsonify({"message": "NOT Hello, World!"}), 200
 
-# Expose WSGI app as 'app'
+@app.route('/news/<info>', methods=['GET'])
+def news(info):
+    base_url = "https://api.webz.io/newsApiLite?token=b5d22bb0-af58-4e7f-b59f-d3d4c02c5ee4&q="
+    url = base_url + info
+    
+    # Make the request to the external news API
+    r = requests.get(url)
+    
+    if r.status_code == 200:
+        data = r.json()
+        return jsonify(data), 200  # Use 200 status code for successful request
+    else:
+        return jsonify({"error": "Failed to fetch news"}), r.status_code  # Return appropriate error if request fails
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
